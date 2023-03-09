b9b0ddadf327fc4b1ed8c51504cd6b5ef89cdeb7
Castor Köhler <castor.kohler@sensible4.fi>
2023-03-09 17:08:59 +0200

    Remove obsolete generate grid vol 2

--- generator/grid.py
+++ generator/grid.py
@@ -1,30 +1,7 @@
 from PIL import Image
 import math
-import os
-import glob
-
 from utils import config
 
-
-def generate_grid():
-    allCardPaths = []
-    tiers = filter(lambda d: '.' not in d, os.listdir(config.CARD_SAVE_DIR))
-    for tier in tiers:
-        cards = sorted(glob.glob(os.path.join(
-            config.CARD_SAVE_DIR, tier, '*.png')))
-        allCardPaths.extend(cards)
-        print(f'Generating grid for TIER: {tier.upper()}')
-        # Process batches of 70
-        for i in range(math.ceil(len(cards) / config.MAX_CARDS)):
-            j = (i+1)*config.MAX_CARDS if (i+1) * \
-                config.MAX_CARDS < len(cards) else len(cards)
-            card_grid, grid_size = combine_cards(cards[i*config.MAX_CARDS:j])
-            name = f'{tier}_{i+1}_{grid_size[0]//config.WIDTH}x{grid_size[1]//config.HEIGHT}_{len(cards)}.png'
-            print(f'Saving {name}')
-            card_grid.save(os.path.join(config.CARD_SAVE_DIR, name))
-
-    print(f'Generating PDF with all cards')
-    combine_cards(allCardPaths)[0].save(config.CARD_SAVE_DIR + "/all_cards.pdf", "PDF" , resolution=300.0)
 
 def combine_cards(cards):
     images = [Image.open(c) for c in cards]
@@ -36,14 +13,15 @@ def combine_cards(cards):
                      nof_rows * config.CARD_HEIGHT_PIXELS)
     else:
         # Add an extra row to grids with only one row, required by Tabletop Simulator
-        grid_size = (config.CARD_WIDTH_PIXELS * len(images), config.CARD_HEIGHT_PIXELS * 2)
+        grid_size = (config.CARD_WIDTH_PIXELS * len(images),
+                     config.CARD_HEIGHT_PIXELS * 2)
     card_grid = Image.new('RGB', grid_size)
 
     # Process batches of MAX_GRID_WIDTH
     for i in range(nof_rows):
         j = (i+1)*config.MAX_GRID_WIDTH if (i+1) * \
             config.MAX_GRID_WIDTH < len(images) else len(images)
-        row = combine_rows(images[i*config.MAX_GRID_WIDTH:j])
+        row = combine_rows(images[i * config.MAX_GRID_WIDTH:j])
         row_height = row.size[1]
         card_grid.paste(row, (0, i * row_height))
     return card_grid, grid_size
