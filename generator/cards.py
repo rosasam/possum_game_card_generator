e0cecfd1533fc26ce4d19027063f67e5541694c3
Castor Köhler <castor.kohler@sensible4.fi>
2023-03-09 13:11:52 +0200

    Change card generator to resize image to config

--- generator/cards.py
+++ generator/cards.py
@@ -8,7 +8,8 @@ from utils.tiers import get_tier_name
 
 # Generates and saves a single card
 def generate_card(tier, name, description, flavour, picturepath, filename):
-    card = Image.new('RGBA', (config.WIDTH, config.HEIGHT))
+    card = Image.new('RGBA',
+                     (config.CARD_WIDTH_PIXELS, config.CARD_HEIGHT_PIXELS))
 
     add_background(card, 'bottom', tier)
     if picturepath:
@@ -25,8 +26,7 @@ def generate_card(tier, name, description, flavour, picturepath, filename):
     write_title(d, name, tier)
 
     if description:
-        description_bottom_y = write_description(
-            d, description, tier)
+        description_bottom_y = write_description(d, description, tier)
     else:
         description_bottom_y = 0
     if flavour:
@@ -41,27 +41,30 @@ def add_background(image, layer, tier):
     elif layer == 'top':
         tier_name = get_tier_name(tier)
         filename = f'card_template_top_{tier_name}.png'
-    background = Image.open(os.path.join(config.TEMPLATES_DIR, filename))
-    assert background.size == (700, 1000)
+    background = Image.open(os.path.join(config.TEMPLATES_DIR,
+                                         filename)).convert('RGBA').resize(
+                                             (config.CARD_WIDTH_PIXELS,
+                                              config.CARD_HEIGHT_PIXELS))
     image.paste(background, (0, 0), background)
 
 
 def add_picture(img, path):
-    picture = Image.new(
-        'RGBA', (config.PIC_WIDTH, config.PIC_HEIGHT), color='red')
-    picture = Image.open(path).convert('RGBA')
+    picture = Image.new('RGBA', (config.PIC_WIDTH, config.PIC_HEIGHT),
+                        color='red')
+    picture = Image.open(path).convert('RGBA').resize(
+        (config.PIC_WIDTH, config.PIC_HEIGHT))
     width, height = picture.size
 
-    left = (width - config.PIC_WIDTH)//2
-    top = (height - config.PIC_HEIGHT)//2
-    right = (width + config.PIC_WIDTH)//2
-    bottom = (height + config.PIC_HEIGHT)//2
+    left = (width - config.PIC_WIDTH) // 2
+    top = (height - config.PIC_HEIGHT) // 2
+    right = (width + config.PIC_WIDTH) // 2
+    bottom = (height + config.PIC_HEIGHT) // 2
 
     # Crop the center of the image
     picture = picture.crop((left, top, right, bottom))
 
-    img.paste(picture, ((config.WIDTH - config.PIC_WIDTH) //
-              2, config.PIC_Y_POSITION), picture)
+    img.paste(picture, ((config.CARD_WIDTH_PIXELS - config.PIC_WIDTH) // 2,
+                        config.PIC_Y_POSITION), picture)
 
 
 def write_title(d, text, tier):
@@ -70,16 +73,16 @@ def write_title(d, text, tier):
     color = 'white' if tier == 4 else 'black'
     font = ImageFont.truetype('fonts/JustAnotherHand-Regular.ttf', fontsize)
     name_width, name_height = font.getsize(text)
-    while name_width > config.WIDTH - config.TEXT_LEFT_MARGIN:
+    while name_width > config.CARD_WIDTH_PIXELS - config.TEXT_LEFT_MARGIN:
         fontsize -= 10
-        font = ImageFont.truetype(
-            'fonts/JustAnotherHand-Regular.ttf', fontsize)
+        font = ImageFont.truetype('fonts/JustAnotherHand-Regular.ttf',
+                                  fontsize)
         name_width, name_height = font.getsize(text)
-    d.text(
-        ((config.WIDTH-name_width)//2, config.TITLE_Y_POSITION + (50-name_height)//2),
-        text,
-        fill=color,
-        font=font)
+    d.text(((config.CARD_WIDTH_PIXELS - name_width) // 2,
+            config.TITLE_Y_POSITION + (50 - name_height) // 2),
+           text,
+           fill=color,
+           font=font)
 
 
 def write_description(d, text, tier):
@@ -98,19 +101,15 @@ def write_description(d, text, tier):
         for i, word in enumerate(words):
             if word.lower() in config.KEYWORDS:
                 word = word if word.isupper() else word.capitalize()
-                font = ImageFont.truetype(
-                    'fonts/RobotoCondensed-Bold.ttf', fontsize)
+                font = ImageFont.truetype('fonts/RobotoCondensed-Bold.ttf',
+                                          fontsize)
             else:
-                font = ImageFont.truetype(
-                    'fonts/RobotoCondensed-Regular.ttf', fontsize)
+                font = ImageFont.truetype('fonts/RobotoCondensed-Regular.ttf',
+                                          fontsize)
 
             width, height = font.getsize(word)
             heights.append(height)
-            d.text(
-                (x_word, y_text),
-                word,
-                fill=color,
-                font=font)
+            d.text((x_word, y_text), word, fill=color, font=font)
 
             # Don't add whitespace if next character is a punctuation mark
             if i + 1 < len(words) and words[i + 1] in ['.', ',']:
@@ -126,14 +125,10 @@ def write_flavour(d, text, start_height, tier):
     color = 'white' if tier == 4 else 'black'
     font = ImageFont.truetype('fonts/RobotoCondensed-Italic.ttf', fontsize)
 
-    lines = textwrap.wrap(text, width=config.CHARACTERS_PER_ROW+4)
+    lines = textwrap.wrap(text, width=config.CHARACTERS_PER_ROW + 4)
     y_text = config.FLAVOUR_START_HEIGHT if config.FLAVOUR_START_HEIGHT > start_height + \
         60 else start_height + 60
     for line in lines:
         width, height = font.getsize(line)
-        d.text(
-            (config.TEXT_LEFT_MARGIN, y_text),
-            line,
-            fill=color,
-            font=font)
+        d.text((config.TEXT_LEFT_MARGIN, y_text), line, fill=color, font=font)
         y_text += height
