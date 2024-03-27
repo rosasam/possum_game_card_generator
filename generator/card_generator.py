7209b24f6e68954bfa705114515dd53bd406f98f
Samuel Rosas <ext-samuel.rosas@sok.fi>
2024-03-27 10:42:50 +0200

    Formatting, event card fixes

--- generator/card_generator.py
+++ generator/card_generator.py
@@ -1,17 +1,31 @@
 import os
 from utils import config
-from generator.cards import add_layer, add_card_type_icon, add_picture, write_description, write_event_description, write_event_title, write_title, write_nut_cost, write_card_type
+from generator.cards import (
+    add_description_picture,
+    add_layer,
+    add_card_type_icon,
+    add_picture,
+    write_description,
+    write_event_description,
+    write_event_title,
+    write_title,
+    write_nut_cost,
+    write_card_type,
+)
 from generator.card import Card
 from fpdf import FPDF
 from PIL import Image, ImageDraw
 import math
 
+
 class Generator:
 
     def __init__(self, options):
         self.cards = []
-        self.testmode = options['testmode'] if 'testmode' in options else False
-        self.no_cardback_mode = options['no_cardback_mode'] if 'no_cardback_mode' in options else False
+        self.testmode = options["testmode"] if "testmode" in options else False
+        self.no_cardback_mode = (
+            options["no_cardback_mode"] if "no_cardback_mode" in options else False
+        )
 
     def add_card(self, card: Card):
         self.cards.append(card)
@@ -26,32 +40,38 @@ class Generator:
     def generate_card(self, card: Card, full_output_path: str):
         # TODO: Split into more functions (maybe split into image manipulator class?)
         card_image = Image.new(
-            'RGBA', (config.CARD_WIDTH_PIXELS, config.CARD_HEIGHT_PIXELS))
+            "RGBA", (config.CARD_WIDTH_PIXELS, config.CARD_HEIGHT_PIXELS)
+        )
 
-        add_layer(card_image, 'bottom', card)
+        add_layer(card_image, "bottom", card)
 
         if card.picture_file_name:
             try:
-                picture_file_path = os.path.join(config.PICTURE_SOURCE_DIR,
-                                                 card.picture_file_name)
+                picture_file_path = os.path.join(
+                    config.PICTURE_SOURCE_DIR, card.picture_file_name
+                )
                 if card.type in config.EVENT_CARD_TYPES:
-                    add_picture(card_image, picture_file_path, config.CARD_HEIGHT_PIXELS, config.CARD_WIDTH_PIXELS)
+                    add_description_picture(
+                        card_image,
+                        picture_file_path,
+                        config.CARD_WIDTH_PIXELS,
+                        config.CARD_HEIGHT_PIXELS,
+                    )
                 else:
                     add_picture(card_image, picture_file_path)
             except Exception as e:
                 print(
-                    f'\033[93mWARNING\033[0m: Picture file \"{picture_file_path}\" for card \"{card.name}\" could not be found.'
+                    f'\033[93mWARNING\033[0m: Picture file "{picture_file_path}" for card "{card.name}" could not be found.'
                 )
-        add_layer(card_image, 'top', card)
+        add_layer(card_image, "top", card)
 
         d = ImageDraw.Draw(card_image)
         if card.type in config.EVENT_CARD_TYPES:
-            print('hehe')
             write_event_title(card_image, card.name)
             write_event_description(card_image, card.description)
         else:
             write_nut_cost(d, card.cost)
-            #add_nuts(card_image, card.cost)
+            # add_nuts(card_image, card.cost)
             write_title(d, card.name)
             write_card_type(d, card)
 
@@ -61,8 +81,8 @@ class Generator:
             if card.type in config.CARD_TYPES_WITH_ICONS:
                 add_card_type_icon(card_image, card.type)
 
-        card_image.save(f'{full_output_path}.png')
-        card.card_image_full_path = f'{full_output_path}.png'
+        card_image.save(f"{full_output_path}.png")
+        card.card_image_full_path = f"{full_output_path}.png"
         if self.testmode:
             card_image.show()
 
@@ -71,11 +91,11 @@ class Generator:
             card_file_name = card.get_card_file_name()
             cardpath = os.path.join(config.CARD_SAVE_DIR, card_file_name)
 
-            print(f'Generating {card_file_name}')
+            print(f"Generating {card_file_name}")
             self.generate_card(card, cardpath)
 
     def generate_grid_pdf(self):
-        pdf = FPDF(orientation="landscape", format="A4", unit='mm')
+        pdf = FPDF(orientation="landscape", format="A4", unit="mm")
         all_card_paths = self.get_all_card_image_file_paths()
         cards_per_page = config.MAX_GRID_WIDTH * config.MAX_GRID_HEIGHT
         total_card_amount = len(all_card_paths)
@@ -90,18 +110,22 @@ class Generator:
                     if j * config.MAX_GRID_WIDTH + i >= card_amount_on_page:
                         break
 
-                    image_position_x = config.X_PRINT_MARGIN_MM + i * config.CARD_WIDTH_MM
-                    index_of_image = page_number * cards_per_page + j * config.MAX_GRID_WIDTH + i
-                    pdf.image(all_card_paths[index_of_image],
-                              x=image_position_x,
-                              y=image_position_y,
-                              w=config.CARD_WIDTH_MM,
-                              h=config.CARD_HEIGHT_MM)
+                    image_position_x = (
+                        config.X_PRINT_MARGIN_MM + i * config.CARD_WIDTH_MM
+                    )
+                    index_of_image = (
+                        page_number * cards_per_page + j * config.MAX_GRID_WIDTH + i
+                    )
+                    pdf.image(
+                        all_card_paths[index_of_image],
+                        x=image_position_x,
+                        y=image_position_y,
+                        w=config.CARD_WIDTH_MM,
+                        h=config.CARD_HEIGHT_MM,
+                    )
             if not self.no_cardback_mode:
                 self.generate_card_back_page(pdf, card_amount_on_page)
-        pdf.output(
-            os.path.join(config.CARD_SAVE_DIR,
-                         config.GRID_PDF_OUTPUT_FILE_NAME))
+        pdf.output(os.path.join(config.CARD_SAVE_DIR, config.GRID_PDF_OUTPUT_FILE_NAME))
 
     def generate_card_back_page(self, pdf, card_amount_on_page):
         # Add card back
@@ -110,8 +134,10 @@ class Generator:
             image_position_y = config.Y_PRINT_MARGIN_MM + j * config.CARD_HEIGHT_MM
             for i in range(config.MAX_GRID_WIDTH):
                 image_position_x = config.X_PRINT_MARGIN_MM + i * config.CARD_WIDTH_MM
-                pdf.image(config.CARD_BACK_IMAGE_FULL_PATH,
-                          x=image_position_x,
-                          y=image_position_y,
-                          w=config.CARD_WIDTH_MM,
-                          h=config.CARD_HEIGHT_MM)
+                pdf.image(
+                    config.CARD_BACK_IMAGE_FULL_PATH,
+                    x=image_position_x,
+                    y=image_position_y,
+                    w=config.CARD_WIDTH_MM,
+                    h=config.CARD_HEIGHT_MM,
+                )
--- generator/cards.py
+++ generator/cards.py
@@ -6,24 +6,26 @@ from generator.word import cap_text_to_max_height, encode_text, text_wrap, write
 from utils import config
 from utils.align import get_description_blocks_y_position, get_description_width
 
+
 def add_layer(image, layer, card):
-    if layer == 'bottom':
-        filename = 'card_template_bot.png'
-    elif layer == 'top':
-        filename = f"card_template_top_{card.type}{'' if card.cost > 0 else '_base'}.png"
-        
-    layerImage = Image.open(os.path.join(config.TEMPLATES_DIR,
-                                         filename)).convert('RGBA').resize(
-                                             (config.CARD_WIDTH_PIXELS,
-                                              config.CARD_HEIGHT_PIXELS))
+    if layer == "bottom":
+        filename = "card_template_bot.png"
+    elif layer == "top":
+        filename = (
+            f"card_template_top_{card.type}{'' if card.cost > 0 else '_base'}.png"
+        )
+
+    layerImage = (
+        Image.open(os.path.join(config.TEMPLATES_DIR, filename))
+        .convert("RGBA")
+        .resize((config.CARD_WIDTH_PIXELS, config.CARD_HEIGHT_PIXELS))
+    )
     image.paste(layerImage, (0, 0), layerImage)
 
 
 def add_picture(img, path, pic_width=config.PIC_WIDTH, pic_height=config.PIC_HEIGHT):
-    picture = Image.new('RGBA', (pic_width, pic_height),
-                        color='red')
-    picture = Image.open(path).convert('RGBA').resize(
-        (pic_width, pic_height))
+    picture = Image.new("RGBA", (pic_width, pic_height), color="red")
+    picture = Image.open(path).convert("RGBA").resize((pic_width, pic_height))
     width, height = picture.size
 
     left = (width - pic_width) // 2
@@ -33,108 +35,175 @@ def add_picture(img, path, pic_width=config.PIC_WIDTH, pic_height=config.PIC_HEI
 
     # Crop the center of the image
     picture = picture.crop((left, top, right, bottom))
- 
-    img.paste(picture, ((config.CARD_WIDTH_PIXELS - pic_width) // 2,
-                        config.PIC_Y_POSITION), picture)
+
+    img.paste(
+        picture,
+        ((config.CARD_WIDTH_PIXELS - pic_width) // 2, config.PIC_Y_POSITION),
+        picture,
+    )
+
+
+def add_description_picture(
+    img, path, pic_width=config.PIC_WIDTH, pic_height=config.PIC_HEIGHT
+):
+    picture = Image.new("RGBA", (pic_width, pic_height), color="red")
+    picture = Image.open(path).convert("RGBA").resize((pic_width, pic_height))
+
+    img.paste(
+        picture,
+        (0, 0),
+        picture,
+    )
+
 
 def add_card_type_icon(image, type):
-    filename = f'{type}-large.png'
+    filename = f"{type}-large.png"
     filepath = os.path.join(config.ICONS_DIR, filename)
-    layerImage = Image.open(filepath).convert('RGBA').resize((config.CARD_TYPE_ICON_SIZE_PIXELS, config.CARD_TYPE_ICON_SIZE_PIXELS))
+    layerImage = (
+        Image.open(filepath)
+        .convert("RGBA")
+        .resize((config.CARD_TYPE_ICON_SIZE_PIXELS, config.CARD_TYPE_ICON_SIZE_PIXELS))
+    )
 
-    image.paste(layerImage, (config.CARD_TYPE_ICON_X, config.CARD_TYPE_ICON_Y), layerImage)
+    image.paste(
+        layerImage, (config.CARD_TYPE_ICON_X, config.CARD_TYPE_ICON_Y), layerImage
+    )
+
 
 # Deprecated
 def add_nuts(image, nuts: int):
     if nuts < 1:
         return
-    filename = 'card_template_NUT.png'
+    filename = "card_template_NUT.png"
     filepath = os.path.join(config.TEMPLATES_DIR, filename)
-    layerImage = Image.open(filepath).convert('RGBA').resize((config.CARD_WIDTH_PIXELS, config.CARD_HEIGHT_PIXELS))
+    layerImage = (
+        Image.open(filepath)
+        .convert("RGBA")
+        .resize((config.CARD_WIDTH_PIXELS, config.CARD_HEIGHT_PIXELS))
+    )
 
     for i in range(nuts):
         offset = (nuts - 1) * config.NUT_SPACING // 2
         x = config.NUT_SPACING * (nuts - i - 1) - offset
         image.paste(layerImage, (x, 0), layerImage)
 
+
 def write_nut_cost(d, cost: int):
     if cost < 1:
         return
     fontsize = config.COST_FONT_SIZE
-    font = ImageFont.truetype(os.path.join('fonts', config.COST_FONT_FILE), fontsize)
+    font = ImageFont.truetype(os.path.join("fonts", config.COST_FONT_FILE), fontsize)
     text = str(cost)
     cost_width, cost_height = font.getsize(text)
-    d.text((config.COST_X_POSITION - (cost_width // 2),
-            config.COST_Y_POSITION - cost_height // 2),
-           text,
-           fill='black',
-           font=font) 
+    d.text(
+        (
+            config.COST_X_POSITION - (cost_width // 2),
+            config.COST_Y_POSITION - cost_height // 2,
+        ),
+        text,
+        fill="black",
+        font=font,
+    )
+
 
 def write_title(d, text):
     text = text.upper()
     fontsize = config.TITLE_FONTSIZE
-    font = ImageFont.truetype(os.path.join('fonts', config.TITLE_FONT_FILE), fontsize)
+    font = ImageFont.truetype(os.path.join("fonts", config.TITLE_FONT_FILE), fontsize)
     name_width, name_height = font.getsize(text)
     while name_width > config.CARD_WIDTH_PIXELS - config.TITLE_MARGIN * 2:
         fontsize -= 2
-        font = ImageFont.truetype(os.path.join('fonts', config.TITLE_FONT_FILE),
-                                  fontsize)
+        font = ImageFont.truetype(
+            os.path.join("fonts", config.TITLE_FONT_FILE), fontsize
+        )
         name_width, name_height = font.getsize(text)
-    d.text(((config.CARD_WIDTH_PIXELS - name_width) // 2,
-            config.TITLE_Y_POSITION - name_height // 2),
-           text,
-           fill='black',
-           font=font)
+    d.text(
+        (
+            (config.CARD_WIDTH_PIXELS - name_width) // 2,
+            config.TITLE_Y_POSITION - name_height // 2,
+        ),
+        text,
+        fill="black",
+        font=font,
+    )
+
 
 def write_description(image, d, text):
     blocks = create_blocks(text)
     block_start_positions = get_description_blocks_y_position(blocks)
 
     for block, block_y_position in zip(blocks, block_start_positions):
-        block.render(image, d, block_y_position) 
-    return block_start_positions[-1] + blocks[-1].height if len(blocks) > 0 else config.DESCRIPTION_Y_POSITION
+        block.render(image, d, block_y_position)
+    return (
+        block_start_positions[-1] + blocks[-1].height
+        if len(blocks) > 0
+        else config.DESCRIPTION_Y_POSITION
+    )
 
 
 def write_card_type(d, card):
     type = card.type.upper()
     base = card.cost < 0
-    if card.type == 'event':
+    if card.type == "event":
         return
-    font = ImageFont.truetype(os.path.join('fonts', config.CARD_TYPE_TEXT_FONT), config.CARD_TYPE_TEXT_FONT_SIZE)
+    font = ImageFont.truetype(
+        os.path.join("fonts", config.CARD_TYPE_TEXT_FONT),
+        config.CARD_TYPE_TEXT_FONT_SIZE,
+    )
     width, _ = font.getsize(type)
     x_pos = config.CARD_TYPE_TEXT_X_POSITION - (width // 2)
-    d.text((x_pos, config.CARD_TYPE_TEXT_Y_POSITION), type, fill='black', font=font)
+    d.text((x_pos, config.CARD_TYPE_TEXT_Y_POSITION), type, fill="black", font=font)
 
 
 # Deprecated
 def write_lock_modifier(d, modifier: str):
-    font = ImageFont.truetype(os.path.join('fonts', config.TITLE_FONT_FILE), 140)
+    font = ImageFont.truetype(os.path.join("fonts", config.TITLE_FONT_FILE), 140)
     width, height = font.getsize(modifier)
-    d.text((65 - width // 2, 95 - height // 2), modifier, fill='#4d4d4d', font=font)
+    d.text((65 - width // 2, 95 - height // 2), modifier, fill="#4d4d4d", font=font)
+
 
 def write_event_title(img, name: str):
-    rotated_img = Image.new("RGBA", (config.CARD_HEIGHT_PIXELS, config.CARD_WIDTH_PIXELS), (0,0,0,0))
+    rotated_img = Image.new(
+        "RGBA", (config.CARD_HEIGHT_PIXELS, config.CARD_WIDTH_PIXELS), (0, 0, 0, 0)
+    )
     d = ImageDraw.Draw(rotated_img)
-    font = ImageFont.truetype(os.path.join('fonts', config.TITLE_FONT_FILE), config.EVENT_TITLE_FONTSIZE)
+    font = ImageFont.truetype(
+        os.path.join("fonts", config.TITLE_FONT_FILE), config.EVENT_TITLE_FONTSIZE
+    )
     name_width, name_height = font.getsize(name)
-    d.text(((config.CARD_HEIGHT_PIXELS - name_width) // 2,
-             config.EVENT_TITLE_Y - name_height // 2),
-             name,
-             fill='white',
-             font=font,
-             stroke_width=3,
-             stroke_fill='black'
-             )
-    #d.rectangle((0,0, config.CARD_HEIGHT_PIXELS, config.CARD_WIDTH_PIXELS), fill=None, outline='green', width=2)
+    d.text(
+        (
+            (config.CARD_HEIGHT_PIXELS - name_width) // 2,
+            config.EVENT_TITLE_Y - name_height // 2,
+        ),
+        name,
+        fill="white",
+        font=font,
+        stroke_width=3,
+        stroke_fill="black",
+    )
+    # d.rectangle((0,0, config.CARD_HEIGHT_PIXELS, config.CARD_WIDTH_PIXELS), fill=None, outline='green', width=2)
     unrotated = rotated_img.rotate(90, expand=True)
     img.paste(unrotated, None, unrotated)
+
 
 def write_event_description(img, text: str):
     EVENT_DESCRIPTION_WIDTH = 600
     EVENT_DESCRIPTION_HEIGHT = 120
-    rotated_img = Image.new("RGBA", (config.CARD_HEIGHT_PIXELS, config.CARD_WIDTH_PIXELS), (0,0,0,0))
+    rotated_img = Image.new(
+        "RGBA", (config.CARD_HEIGHT_PIXELS, config.CARD_WIDTH_PIXELS), (0, 0, 0, 0)
+    )
     d = ImageDraw.Draw(rotated_img)
-    paragraphs, line_height = cap_text_to_max_height(text, EVENT_DESCRIPTION_HEIGHT, EVENT_DESCRIPTION_WIDTH) 
-    write_text(d, paragraphs, (config.CARD_HEIGHT_PIXELS - EVENT_DESCRIPTION_WIDTH) // 2, config.EVENT_DESCRIPTION_Y, EVENT_DESCRIPTION_WIDTH, line_height=line_height)
+    paragraphs, line_height = cap_text_to_max_height(
+        text, EVENT_DESCRIPTION_HEIGHT, EVENT_DESCRIPTION_WIDTH
+    )
+    write_text(
+        d,
+        paragraphs,
+        (config.CARD_HEIGHT_PIXELS - EVENT_DESCRIPTION_WIDTH) // 2,
+        config.EVENT_DESCRIPTION_Y,
+        EVENT_DESCRIPTION_WIDTH,
+        line_height=line_height,
+    )
     unrotated = rotated_img.rotate(90, expand=True)
     img.paste(unrotated, None, unrotated)
